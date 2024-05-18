7417306c7cc16b1df36426178821079bd3981a7c
Khalidgithub2020331007 <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-05-18 22:14:56 +0600

    Update final code.ino

--- final code.ino
+++ final code.ino
@@ -1,93 +1,109 @@
 #include <AFMotor.h>
-
-// Initial motors pin
+ 
+//initial motors pin
 AF_DCMotor motor1(1, MOTOR12_1KHZ);
 AF_DCMotor motor3(3, MOTOR34_1KHZ);
-
+ 
+int val;
+int Speeed = 255;
 const int trigPin = 7;
 const int echoPin = 8;
 
-int val;
 int Speed = 255;
 long duration;
 int distance;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  pinMode(trigPin, OUTPUT);
+    pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
-
-void loop() {
-  if (Serial.available() > 0) {
+void loop(){
+  if(Serial.available() > 0){
     val = Serial.read();
-
+     
     Stop();
+     
+          if (val == 'F'){
+          forward();
+          }
+ 
+          if (val == 'B'){
+          back();
+          }
+ 
+          if (val == 'L'){
+          left();
+          }
+ 
+          if (val == 'R'){
+          right();
+          }
 
-    if (val == 'F') {
-      forward();
-    }
 
-    if (val == 'B') {
-      back();
-    }
-
-    if (val == 'L') {
-      left();
-    }
-
-    if (val == 'R') {
-      right();
-    }
-
-    if (val == 'T') {
-      Stop();
-    }
+          if (val == 'T'){
+          Stop();
+          }
   }
-  
-  // Check for obstacles
-  distance = getDistance();
-  if (distance > 0 && distance <= 20) { // If an obstacle is within 20 cm
-    Stop();
-  }
-}
+             distance = getDistance();
+                Serial.println(distance);
+            
 
-void forward() {
-  motor1.setSpeed(Speed);
-  motor1.run(FORWARD);
-  motor3.setSpeed(253);
-  motor3.run(FORWARD);
 }
-
-void back() {
-  motor1.setSpeed(Speed);
-  motor1.run(BACKWARD);
-  motor3.setSpeed(253);
+          
+ 
+           
+ 
+ 
+ 
+void forward()
+{
+  motor1.setSpeed(Speeed); 
+  motor1.run(FORWARD); 
+  motor3.setSpeed(252);
+  motor3.run(FORWARD); 
+}
+ 
+void back()
+{
+  motor1.setSpeed(Speeed); 
+  motor1.run(BACKWARD);   
+  motor3.setSpeed(252);
   motor3.run(BACKWARD);
-}
-
-void left() {
-  motor1.setSpeed(0);
-  motor1.run(RELEASE);
   
-  motor3.setSpeed(Speed);
-  motor3.run(253);
 }
-
-void right() {
-  motor1.setSpeed(Speed);
-  motor1.run(FORWARD);
+ 
+void left()
+{
+  motor1.setSpeed(0); //Define maximum velocity
+  motor1.run(RELEASE); //rotate the motor anti-clockwise
   
-  motor3.setSpeed(0);
-  motor3.run(RELEASE);
+  motor3.setSpeed(Speeed); //Define maximum velocity
+  motor3.run(FORWARD);  //rotate the motor clockwise
 }
+ 
+void right()
+{
+  motor1.setSpeed(Speeed); //Define maximum velocity
+  motor1.run(FORWARD); //rotate the motor clockwise
+ 
+  motor3.setSpeed(0); //Define maximum velocity
+  motor3.run(RELEASE); //rotate the motor anti-clockwise
+ 
+}
+ 
 
-void Stop() {
-  motor1.setSpeed(0);
-  motor1.run(RELEASE);
+ 
+ 
+void Stop()
+{
+  motor1.setSpeed(0); //Define minimum velocity
+  motor1.run(RELEASE); //stop the motor when release the button
   
-  motor3.setSpeed(0);
-  motor3.run(RELEASE);
+  motor3.setSpeed(0); //Define minimum velocity
+  motor3.run(RELEASE); //stop the motor when release the button
+ 
 }
 
 int getDistance() {
@@ -103,8 +119,9 @@ int getDistance() {
   
   duration = pulseIn(echoPin, HIGH);
 
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+
+  distance = duration * 0.034 / 2; 
+
 
   return distance;
 }

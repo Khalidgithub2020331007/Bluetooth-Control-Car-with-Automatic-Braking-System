76ccb7babe53a48eb2e6c1ce42cd12b1aefbea01
Khalidgithub2020331007 <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-05-21 07:09:38 +0600

    Update combinecode.ino

--- combinecode.ino
+++ combinecode.ino
@@ -1,67 +1,67 @@
 #include <AFMotor.h>
+#include <SoftwareSerial.h>
 
-#define echoPin 8 
-#define trigPin 7  
-
-long duration, distance;
+// Define motor ports on the Adafruit Motor Shield
 AF_DCMotor motor1(1, MOTOR12_1KHZ);
 AF_DCMotor motor3(3, MOTOR34_1KHZ);
 
-int val;
-int Speeed = 255;
+// Define Bluetooth module pins (using SoftwareSerial)
+SoftwareSerial Bluetooth(2, 3); // RX, TX
+
+// Define speed for the motors
+const int SPEED = 255;
+
+// Define ultrasonic sensor pins
+#define trigPin 7
+#define echoPin 8
 
 void setup() {
   Serial.begin(9600);
+  Bluetooth.begin(9600);
+  Serial.println("Motor, Bluetooth, and Ultrasonic Test");
+  
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  // Ultrasonic sensor code
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  long distance = getDistance();
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
 
-  duration = pulseIn(echoPin, HIGH);
-  distance = duration / 58.2;
-
-  String disp = String(distance);
-  Serial.print("distance :");
-  Serial.print(disp);
-  Serial.print(" cm");
-  Serial.println("");
-
-  // Stop the car if distance is less than 20 cm
-  if (distance < 20) {
-    back();  // Changed 'Back()' to 'back()'
+  if (distance < 10) {
+    Serial.println("Obstacle detected! Stopping and reversing...");
+    Stop();
     delay(2000);
+    back();
+    delay(2000);
+    Stop();
   } else {
-    // Check for user input if distance is safe
-    if (Serial.available() > 0) {
-      val = Serial.read();
+    if (Bluetooth.available() > 0) {
+      char command = Bluetooth.read();
+      Serial.print("Received: ");
+      Serial.println(command);
 
-      Stop();
-
-      if (val == 'F') {
-        forward();
-      }
-
-      if (val == 'B') {
-        back();
-      }
-
-      if (val == 'L') {
-        left();
-      }
-
-      if (val == 'R') {
-        right();
-      }
-
-      if (val == 'T') {
-        Stop();
+      switch (command) {
+        case 'F':
+          forward();
+          break;
+        case 'B':
+          back();
+          break;
+        case 'L':
+          left();
+          break;
+        case 'R':
+          right();
+          break;
+        case 'T':
+          Stop();
+          break;
+        default:
+          Serial.println("Invalid command");
+          break;
       }
     }
   }
@@ -69,37 +69,54 @@ void loop() {
   delay(100);
 }
 
+long getDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = duration / 58.2;
+  return distance;
+}
+
 void forward() {
-  motor1.setSpeed(Speeed);
+  Serial.println("Forward");
+  motor1.setSpeed(SPEED);
   motor1.run(FORWARD);
-  motor3.setSpeed(252);
+  motor3.setSpeed(SPEED);
   motor3.run(FORWARD);
 }
 
 void back() {
-  motor1.setSpeed(Speeed);
+  Serial.println("Backward");
+  motor1.setSpeed(SPEED);
   motor1.run(BACKWARD);
-  motor3.setSpeed(252);
+  motor3.setSpeed(SPEED);
   motor3.run(BACKWARD);
 }
 
 void left() {
-  motor1.setSpeed(0); // Stop the left motor
+  Serial.println("Left");
+  motor1.setSpeed(0);
   motor1.run(RELEASE);
-  motor3.setSpeed(Speeed); // Move the right motor forward
+  motor3.setSpeed(SPEED);
   motor3.run(FORWARD);
 }
 
 void right() {
-  motor1.setSpeed(Speeed); // Move the left motor forward
+  Serial.println("Right");
+  motor1.setSpeed(SPEED);
   motor1.run(FORWARD);
-  motor3.setSpeed(0); // Stop the right motor
+  motor3.setSpeed(0);
   motor3.run(RELEASE);
 }
 
 void Stop() {
-  motor1.setSpeed(0); // Stop the left motor
+  Serial.println("Stop");
+  motor1.setSpeed(0);
   motor1.run(RELEASE);
-  motor3.setSpeed(0); // Stop the right motor
+  motor3.setSpeed(0);
   motor3.run(RELEASE);
 }
